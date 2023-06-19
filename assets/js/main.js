afa0cf3be485f45f0d2f0129b2999afbca0c510c
ccechatelier <corentinchatelier@gmail.com>
2023-06-19 16:38:44 +0200

    Fix bug closing

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,19 +11,22 @@ if(navToggle){
     })
 }
 
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-
-
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
-
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 /*==================== ACCORDION SKILLS ====================*/
 
 
