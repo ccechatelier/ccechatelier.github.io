54257acb56d9d31a411a829eda71aee58abefba5
ccechatelier <corentinchatelier@gmail.com>
2024-05-30 18:11:54 +0200

    Update scholar.py and scholar_profile.json

--- scholar.py
+++ scholar.py
@@ -51,10 +51,50 @@ for pub in author['publications']:
         "citation": publication['num_citations']
         })
         index += 1
-    
+
+profile_data_bis = {
+    "name": author['name'],
+    "affiliation": author.get('affiliation', 'N/A'),
+    "h_index": author.get('hindex', 'N/A'),
+    "i10_index": author.get('i10index', 'N/A'),
+    "citations": author.get('citedby', 'N/A'),
+    "citations_per_year": author.get('cites_per_year', 'N/A'),
+    "publications": []
+}
+
+index -= 1
+for pub in author['publications']:
+    publication = scholarly.fill(pub)
+
+    if publication['bib'].get('conference', 'N/A') == 'N/A' and publication['bib'].get('publisher', 'N/A') != 'N/A'  and publication['bib'].get('journal', 'N/A') != 'N/A' :
+        print(index, publication['bib'].get('journal', 'N/A'), publication['bib'].get('publisher', 'N/A'), publication['bib'].get('conference', 'N/A'))
+        profile_data_bis["publications"].append({
+        "index": index,
+        "title": publication['bib']['title'],
+        "journal": publication['bib'].get('journal', 'N/A'),
+        "publisher": publication['bib'].get('publisher', 'N/A'),
+        "conference": publication['bib'].get('conference', 'N/A'),
+        "year": publication['bib'].get('pub_year', 'N/A'),
+        "citation": publication['num_citations']
+        })
+        index -= 1
+        
+    if publication['bib'].get('conference', 'N/A') == 'N/A' and publication['bib'].get('publisher', 'N/A') == 'N/A'  and publication['bib'].get('journal', 'N/A') == 'N/A' :
+        print(index, publication['bib'].get('journal', 'N/A'), publication['bib'].get('publisher', 'N/A'), publication['bib'].get('conference', 'N/A'))
+        profile_data_bis["publications"].append({
+        "index": index,
+        "title": publication['bib']['title'],
+        "journal": publication['bib'].get('journal', 'N/A'),
+        "publisher": publication['bib'].get('publisher', 'N/A'),
+        "conference": publication['bib'].get('conference', 'N/A'),
+        "year": publication['bib'].get('pub_year', 'N/A'),
+        "citation": publication['num_citations']
+        })
+        index -= 1
+
 print("[INFO] Saving the data ...")
 # Save the data as a JSON file
 with open('scholar_profile.json', 'w') as f:
-    json.dump(profile_data, f)
+    json.dump(profile_data_bis, f)
 
 print("Profile data saved to scholar_profile.json")
