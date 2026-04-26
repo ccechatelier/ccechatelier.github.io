0020c31b9c468fd0d7860f0fa47719dd7d47a827
ccechatelier <corentinchatelier@gmail.com>
2026-04-26 16:36:48 +0200

    Update GS data and add Pub22/Pub23

--- scholar.py
+++ scholar.py
@@ -28,7 +28,7 @@ profile_data = {
 
 index = 1
 for pub in author['publications']:
-    # publication = scholarly.fill(pub)
+    publication = scholarly.fill(pub)
     publication = pub
     if publication['bib'].get('conference', 'N/A') == 'N/A' and publication['bib'].get('publisher', 'N/A') != 'N/A'  and publication['bib'].get('journal', 'N/A') != 'N/A' :
         print(index, publication['bib'].get('journal', 'N/A'), publication['bib'].get('publisher', 'N/A'), publication['bib'].get('conference', 'N/A'))
@@ -56,54 +56,21 @@ for pub in author['publications']:
         })
         index += 1
 
-profile_data_bis = {
-    "name": author['name'],
-    "affiliation": author.get('affiliation', 'N/A'),
-    "h_index": author.get('hindex', 'N/A'),
-    "i10_index": author.get('i10index', 'N/A'),
-    "citations": author.get('citedby', 'N/A'),
-    "citations_per_year": author.get('cites_per_year', 'N/A'),
-    "publications": []
-}
-
 print("[INFO] Saving the data ...")
 # Save the data as a JSON file
 with open('scholar_profile_intermediaire.json', 'w') as f:
     json.dump(profile_data, f)
 
-index -= 1
-for pub in author['publications']:
-    # publication = scholarly.fill(pub)
-    publication = pub 
-    if publication['bib'].get('conference', 'N/A') == 'N/A' and publication['bib'].get('publisher', 'N/A') != 'N/A'  and publication['bib'].get('journal', 'N/A') != 'N/A' :
-        print(index, publication['bib'].get('journal', 'N/A'), publication['bib'].get('publisher', 'N/A'), publication['bib'].get('conference', 'N/A'))
-        profile_data_bis["publications"].append({
-        "index": index,
-        "title": publication['bib']['title'],
-        "journal": publication['bib'].get('journal', 'N/A'),
-        "publisher": publication['bib'].get('publisher', 'N/A'),
-        "conference": publication['bib'].get('conference', 'N/A'),
-        "year": publication['bib'].get('pub_year', 'N/A'),
-        "citation": publication['num_citations']
-        })
-        index -= 1
-        
-    if publication['bib'].get('conference', 'N/A') == 'N/A' and publication['bib'].get('publisher', 'N/A') == 'N/A'  and publication['bib'].get('journal', 'N/A') == 'N/A' :
-        print(index, publication['bib'].get('journal', 'N/A'), publication['bib'].get('publisher', 'N/A'), publication['bib'].get('conference', 'N/A'))
-        profile_data_bis["publications"].append({
-        "index": index,
-        "title": publication['bib']['title'],
-        "journal": publication['bib'].get('journal', 'N/A'),
-        "publisher": publication['bib'].get('publisher', 'N/A'),
-        "conference": publication['bib'].get('conference', 'N/A'),
-        "year": publication['bib'].get('pub_year', 'N/A'),
-        "citation": publication['num_citations']
-        })
-        index -= 1
+with open('scholar_profile_intermediaire.json', 'r') as file:
+    data = json.load(file)
+    publications = data['publications']
 
-print("[INFO] Saving the data ...")
-# Save the data as a JSON file
-with open('scholar_profile.json', 'w') as f:
-    json.dump(profile_data_bis, f)
+for pub in publications:
+    pub['index'] = len(publications) - pub['index'] + 1
 
-print("Profile data saved to scholar_profile.json")
+# Save the updated publications data to the JSON file
+with open('scholar_profile_v2.json', 'w') as f:
+    data['publications'] = publications
+    json.dump(data, f, indent=2)
+
+print("[INFO] Updated scholar_profile_v2.json with new publication indices")
